c49aec984e955d3798e5fb92efa4b452a2d41c93
Merlin Forsman <forsman93@hotmail.com>
2019-10-30 08:13:03 +0100

    more controllers

---
+++ WebApplication1/WebApplication1/Controllers/FreelancersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FreelancersController : Controller
+    {
+        private DataBankEntities db = new DataBankEntities();
+
+        // GET: Freelancers
+        public ActionResult Index()
+        {
+            var freelancer = db.Freelancer.Include(f => f.PersonalInformation);
+            return View(freelancer.ToList());
+        }
+
+        // GET: Freelancers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Freelancer freelancer = db.Freelancer.Find(id);
+            if (freelancer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(freelancer);
+        }
+
+        // GET: Freelancers/Create
+        public ActionResult Create()
+        {
+            ViewBag.PersonalInformation_ID = new SelectList(db.PersonalInformation, "PersonalInformation_ID", "FirstName");
+            return View();
+        }
+
+        // POST: Freelancers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Freelancer_ID,PersonalInformation_ID")] Freelancer freelancer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Freelancer.Add(freelancer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PersonalInformation_ID = new SelectList(db.PersonalInformation, "PersonalInformation_ID", "FirstName", freelancer.PersonalInformation_ID);
+            return View(freelancer);
+        }
+
+        // GET: Freelancers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Freelancer freelancer = db.Freelancer.Find(id);
+            if (freelancer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PersonalInformation_ID = new SelectList(db.PersonalInformation, "PersonalInformation_ID", "FirstName", freelancer.PersonalInformation_ID);
+            return View(freelancer);
+        }
+
+        // POST: Freelancers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Freelancer_ID,PersonalInformation_ID")] Freelancer freelancer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(freelancer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PersonalInformation_ID = new SelectList(db.PersonalInformation, "PersonalInformation_ID", "FirstName", freelancer.PersonalInformation_ID);
+            return View(freelancer);
+        }
+
+        // GET: Freelancers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Freelancer freelancer = db.Freelancer.Find(id);
+            if (freelancer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(freelancer);
+        }
+
+        // POST: Freelancers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Freelancer freelancer = db.Freelancer.Find(id);
+            db.Freelancer.Remove(freelancer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
